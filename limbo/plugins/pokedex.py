1b752472264955e63dfd7cb7ecdcd7c683976f23
Shyam S Kumar <shyam.salim.kumar@outlook.com>
2015-06-27 04:25:17 +0530

    Surrounded with a try/except

--- limbo/plugins/pokedex.py
+++ limbo/plugins/pokedex.py
@@ -14,37 +14,39 @@ def pokedex(name):
     url = "http://pokemondb.net/pokedex/{0}".format(name)
     soup = BeautifulSoup(requests.get(url).text)
 
+    message = ":monkey: Sorry, no matching pokemon found :monkey:"
     heading = name[0].upper() + name[1:]
-    description = soup.find("div", attrs={"class": "col desk-span-8 lap-span-6"}).text \
-        .encode("utf8").replace("Pok\xc3\x83\xc2\xa9mon", "Pokemon").replace("\n", "").replace("\t", "")
-    basic_tabs = soup.find("ul", attrs={"class": "svtabs-panel-list"})
-    tabs = basic_tabs.findAll("li", attrs={"class": "svtabs-panel"})
-    current_tab = tabs[0]
-    img_url = current_tab.find("div", attrs={"class": "col desk-span-4 lap-span-6 figure"}).find("img").get("src") \
-        .encode("utf8")
-    vitals_table = current_tab.find("table", attrs={"class": "vitals-table"})
-    trs = vitals_table.findAll("tr")
-    vitals = {}
 
-    for x in range(0, 6):
-        current_tr = trs[x]
-        key = "id" if x == 0 else current_tr.find("th").text.encode("utf8")
+    try:
+        description = soup.find("div", attrs={"class": "col desk-span-8 lap-span-6"}).text.strip()
+        basic_tabs = soup.find("ul", attrs={"class": "svtabs-panel-list"})
+        tabs = basic_tabs.findAll("li", attrs={"class": "svtabs-panel"})
+        current_tab = tabs[0]
+        img_url = current_tab.find("div", attrs={"class": "col desk-span-4 lap-span-6 figure"}).find("img").get("src")
+        vitals_table = current_tab.find("table", attrs={"class": "vitals-table"})
+        trs = vitals_table.findAll("tr")
+        keys = []
+        vitals = {}
 
-        if x == 0:
-            vitals[key] = current_tr.find("strong").text.encode("utf8")
-        elif x == 1 or x == 5:
-            vitals[key] = ",".join([link.text.encode("utf8") for link in current_tr.findAll("a")])
-        else:
-            vitals[key] = current_tr.find("td").text.encode("utf8")
+        for x in range(0, 6):
+            current_tr = trs[x]
+            key = "Id" if x == 0 else current_tr.find("th").text
+            keys.append(key)
 
-    if not description:
-        return ":monkey: Sorry, no matching pokemon found :monkey:"
+            if x == 0:
+                vitals[key] = current_tr.find("strong").text
+            elif x == 1 or x == 5:
+                vitals[key] = ",".join([link.text for link in current_tr.findAll("a")])
+            else:
+                vitals[key] = current_tr.find("td").text
 
-    message = Template("*$heading*\nDescription: _$description_\n$img\n$vital")\
-        .substitute(heading=heading, description=description, img=img_url,
-                    vital="\n".join([":".join([x, vitals[x]]) for x in vitals]))
-    return message
-
+        message = Template("*$heading*\nDescription: $description\n$img\n$vital")\
+            .substitute(heading=heading, description="_" + description + "_", img=img_url,
+                        vital="\n".join([": ".join([x, vitals[x]]) for x in keys]))
+    except AttributeError:
+        print("Page parsing failed. 404 or API change")
+    finally:
+        return message
 
 def on_message(msg, server):
     text = msg.get("text", "")
